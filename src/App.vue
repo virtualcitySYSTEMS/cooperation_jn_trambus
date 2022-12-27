7ce65fad470dc13a27ed10f5bce789fea80b6807
Ismail Sunni <imajimatika@gmail.com>
2022-12-27 15:10:35 +0700

    Restructure: router only handle the sidepanel's content.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PhotoGallery from '@/components/home/PhotoGallery.vue'
+import MapComponent from '@/components/map/MapComponent.vue'
+
+import SidePanel from '@/components/home/SidePanel.vue'
+import PlanningView from '@/views/PlanningView.vue'
+
+import { usePanelsStore } from '@/stores/panels'
+import HeadToolbarTrambus from '@/components/map/HeadToolbarTrambus.vue'
+
+const panelStore = usePanelsStore()
+</script>
 
 <template>
-  <!--
-  <header class="h-16 bg-green-200 flex flex-row justify-center">
-    <router-link class="mx-5" to="/">Home</router-link>
-    <router-link class="mx-5" to="/line/1">Line</router-link>
-    <router-link class="mx-5" to="/line/1/station/5">Station</router-link>
-  </header>
--->
-  <RouterView />
+  <main class="h-screen flex">
+    <aside class="z-10 absolute">
+      <SidePanel v-show="panelStore.isPlanningViewShown === false">
+        <RouterView />
+      </SidePanel>
+    </aside>
+
+    <HeadToolbarTrambus
+      v-show="panelStore.isPlanningViewShown === false"
+    ></HeadToolbarTrambus>
+
+    <div class="grow">
+      <MapComponent></MapComponent>
+    </div>
+
+    <div class="z-10 absolute inset-x-0 bottom-0 max-w-max m-auto">
+      <PhotoGallery></PhotoGallery>
+    </div>
+
+    <!--Do lazy loading for planning view.
+      Only render it once, and after that 'hide' the planning view so that we can keep its state and no need to load it again. -->
+    <div
+      class="absolute h-screen w-screen"
+      v-if="panelStore.hasPlanningViewRendered"
+      v-show="panelStore.isPlanningViewShown"
+    >
+      <PlanningView></PlanningView>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
--- src/views/LineView.vue
+++ src/views/LineView.vue
@@ -1,13 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header></header>
-  <main class="h-screen flex">
-    <div class="w-72 p-4 bg-blue-300"></div>
-    <div class="p-4">
-      <div>
-        Id is <span class="font-bold"> {{ $route.params.id }}</span>
-      </div>
-    </div>
-  </main>
+  <div>Line View</div>
 </template>
