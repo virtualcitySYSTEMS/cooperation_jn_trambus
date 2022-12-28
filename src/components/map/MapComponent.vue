f677df45f800dc50e545a0eaeb6621123dc18699
Ismail Sunni <imajimatika@gmail.com>
2022-12-28 17:37:37 +0700

    Update layer visibility based on current view (on first loading)

--- src/components/map/MapComponent.vue
+++ src/components/map/MapComponent.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, provide, ref } from 'vue'
-import { CesiumMap, Context, VcsApp } from '@vcmap/core'
+import { CesiumMap, Context, VcsApp, Layer } from '@vcmap/core'
 
 import UiMap from '@/components/ui/UiMap.vue'
 import NavigationButtons from '@/components/map/buttons/NavigationButtons.vue'
@@ -24,6 +24,7 @@ onMounted(async () => {
     cesiumMap.getScene().globe.maximumScreenSpaceError = 1
   }
   appLoaded.value = true
+  await updateLayersVisibility()
 })
 
 // The following code is needed to cleanup resources we created
@@ -33,23 +34,27 @@ onUnmounted(() => {
   app.destroy()
 })
 
-function setLayerVisible(layerName: string, visible: boolean) {
-  const layer = app.maps.layerCollection.getByKey(layerName)
+async function setLayerVisible(layerName: string, visible: boolean) {
+  const layer: Layer = app.maps.layerCollection.getByKey(layerName)
   if (visible) {
-    layer?.activate()
+    await layer?.activate()
   } else if (layer?.active) {
     layer.deactivate()
   }
 }
 
-layerStore.$subscribe(() => {
-  setLayerVisible('metro', layerStore.visibilities.metro)
-  setLayerVisible('bus', layerStore.visibilities.bus)
-  setLayerVisible('bike', layerStore.visibilities.bike)
-  setLayerVisible('trambusLines', layerStore.visibilities.trambusLines)
-  setLayerVisible('trambusStops', layerStore.visibilities.trambusStops)
-  setLayerVisible('parking', layerStore.visibilities.parking)
-  setLayerVisible('poi', layerStore.visibilities.poi)
+async function updateLayersVisibility() {
+  await setLayerVisible('metro', layerStore.visibilities.metro)
+  await setLayerVisible('bus', layerStore.visibilities.bus)
+  await setLayerVisible('bike', layerStore.visibilities.bike)
+  await setLayerVisible('trambusLines', layerStore.visibilities.trambusLines)
+  await setLayerVisible('trambusStops', layerStore.visibilities.trambusStops)
+  await setLayerVisible('parking', layerStore.visibilities.parking)
+  await setLayerVisible('poi', layerStore.visibilities.poi)
+}
+
+layerStore.$subscribe(async () => {
+  await updateLayersVisibility()
 })
 </script>
 
