9d590bc0c514a7b0797743ebacd6eda81106b0f3
Ismail Sunni <imajimatika@gmail.com>
2022-12-30 16:38:11 +0700

    Add 3D store.

--- src/stores/map.ts
+++ src/stores/map.ts
@@ -3,12 +3,27 @@ import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useMapStore = defineStore('map', () => {
+  // Map state
+  const activeMap: Ref<string> = ref('ol') // Map: 'ol', 'cesium'
+  const viewPoint: Ref<string> = ref('rennes') // See the map.config.json
+
   const eventRandomId: Ref<number> = ref(0)
-  const viewPoint: Ref<string> = ref('rennes')
 
   function triggerEvent() {
     eventRandomId.value = Math.random()
   }
 
-  return { triggerEvent, eventRandomId, viewPoint }
+  function is3D() {
+    return activeMap.value == 'cesium'
+  }
+
+  function toggle3D() {
+    if (is3D()) {
+      activeMap.value = 'ol'
+    } else {
+      activeMap.value = 'cesium'
+    }
+  }
+
+  return { triggerEvent, eventRandomId, viewPoint, activeMap, is3D, toggle3D }
 })
