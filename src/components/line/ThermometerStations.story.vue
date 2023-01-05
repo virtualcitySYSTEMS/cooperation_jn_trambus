461079ed7116b78aa40f8e602dc8082f5f9fadce
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-01-05 17:22:15 +0100

    add story for component ThermometerStations

---
+++ src/components/line/ThermometerStations.story.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import ThermometerStations from './ThermometerStations.vue'
+function initState() {
+  return {
+    lineNumber: 1,
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="ThermometerStations"
+    :layout="{
+      type: 'grid',
+      width: 450,
+    }"
+    group="homepage"
+    :init-state="initState"
+  >
+    <template #default="{ state }">
+      <ThermometerStations :line="state.lineNumber"></ThermometerStations>
+    </template>
+  </Story>
+</template>
