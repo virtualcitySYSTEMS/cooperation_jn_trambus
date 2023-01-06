2dd6ea79a86e13e9798e28ba7d27c5744daf5856
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-01-06 13:28:56 +0100

    Create component UiParkingInformations

---
+++ src/components/ui/UiParkingInformation.story.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import UiParkingInformation from './UiParkingInformation.vue'
+function initState() {
+  return {
+    name: 'Parking Lorem',
+    station: 'Station Champs-Daguet',
+    address: '3 avenue Hambourg - 35000 Rennes',
+    place: 300,
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="UiParkingInformation"
+    :layout="{
+      type: 'grid',
+      width: 450,
+    }"
+    group="homepage"
+    :init-state="initState"
+  >
+    <template #default="{ state }">
+      <UiParkingInformation
+        :name="state.name"
+        :station="state.station"
+        :address="state.address"
+        :place="state.place"
+      >
+      </UiParkingInformation>
+    </template>
+  </Story>
+</template>
