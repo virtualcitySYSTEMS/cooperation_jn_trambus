7cc6496a1b61d7d72cdc39a6cf4756a57bb773ff
Ismail Sunni <imajimatika@gmail.com>
2023-01-06 15:06:26 +0700

    Add UiLineHeader story.

---
+++ src/components/ui/UiLineHeader.story.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import UiLineHeader from '@/components/ui/UiLineHeader.vue'
+
+function initState() {
+  return {
+    id: 1,
+    name: 'Ligne T1',
+    start: 'Stade Rennais',
+    end: 'Brécé',
+  }
+}
+</script>
+
+<template>
+  <Story
+    title="UiLineHeader"
+    :layout="{
+      type: 'grid',
+      width: 500,
+    }"
+    group="homepage"
+    :init-state="initState"
+  >
+    <template #default="{ state }">
+      <UiLineHeader
+        :line="state.id"
+        :name="state.name"
+        :start="state.start"
+        :end="state.end"
+      >
+      </UiLineHeader>
+    </template>
+  </Story>
+</template>
