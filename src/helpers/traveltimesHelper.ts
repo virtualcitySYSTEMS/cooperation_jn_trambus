aae9d149e21a5ca9bdea259b3c190b73fce8f63f
Ismail Sunni <imajimatika@gmail.com>
2023-02-01 10:00:34 +0700

    Use existing mercator projection object.

--- src/helpers/traveltimesHelper.ts
+++ src/helpers/traveltimesHelper.ts
@@ -2,7 +2,7 @@ import type { TravelTimeModel } from '@/model/travel-time.model'
 import { RENNES_LAYER } from '@/stores/layers'
 import { LineString } from 'ol/geom'
 import type { RennesApp } from '@/services/RennesApp'
-import { getHeightFromTerrainProvider, Projection } from '@vcmap/core'
+import { getHeightFromTerrainProvider, mercatorProjection } from '@vcmap/core'
 
 export async function lineStringsFromTraveltimes(
   traveltimes: TravelTimeModel[],
@@ -21,13 +21,9 @@ export async function lineStringsFromTraveltimes(
       traveltime.end
     )
 
+    // Add altitude / height for 3D map so that it's not under the ground
     if (is3D) {
       const cesiumMap = rennesApp.get3DMap()
-      const proj3857 = new Projection({
-        epsg: 'EPSG:3857',
-        proj4:
-          '+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs',
-      })
 
       const [startCoordinate, endCoordinate] =
         await getHeightFromTerrainProvider(
@@ -36,7 +32,7 @@ export async function lineStringsFromTraveltimes(
             startTrambusStop?.getGeometry()?.getCoordinates(),
             endTrambusStop?.getGeometry()?.getCoordinates(),
           ],
-          proj3857
+          mercatorProjection
         )
 
       return new LineString([startCoordinate, endCoordinate])
