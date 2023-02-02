4637a108c2594821b25305363eace10db3d314c0
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-02 10:38:22 +0100

    fix: img service not display in online env

--- src/model/services.model.ts
+++ src/model/services.model.ts
@@ -1,3 +1,12 @@
+import imgBikeRack from '@/assets/icons/services/bike-rack.svg'
+import imgBoxBike from '@/assets/icons/services/box-bike.svg'
+import imgNewspaperDistributor from '@/assets/icons/services/newspaper-distributor.svg'
+import imgParcelLocker from '@/assets/icons/services/parcel-locker.svg'
+import imgPublicSanitary from '@/assets/icons/services/public-sanitary.svg'
+import imgTicketMachine from '@/assets/icons/services/ticket-machine.svg'
+import imgUsbCharging from '@/assets/icons/services/usb-charging.svg'
+import imgVegetableGarden from '@/assets/icons/services/vegetable-garden.svg'
+
 export type ServiceType =
   | 'ticket-machine'
   | 'box-bike'
@@ -17,6 +26,17 @@ export const TEXT_OF_SERVICE: Record<ServiceType, string> = {
   'usb-charging': 'Prise recharge USB',
   'parcel-locker': 'Casier livraison colis',
   'public-sanitary': 'Sanitaire public',
+}
+
+export const IMG_OF_SERVICE: Record<ServiceType, string> = {
+  'ticket-machine': imgTicketMachine,
+  'box-bike': imgBoxBike,
+  'newspaper-distributor': imgNewspaperDistributor,
+  'vegetable-garden': imgVegetableGarden,
+  'bike-rack': imgBikeRack,
+  'usb-charging': imgUsbCharging,
+  'parcel-locker': imgParcelLocker,
+  'public-sanitary': imgPublicSanitary,
 }
 
 export interface ServiceModel {
