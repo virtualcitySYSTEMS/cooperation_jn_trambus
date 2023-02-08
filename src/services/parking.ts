dfbf203c678f30d9be247912d0d80bb2adcff82b
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-08 10:33:55 +0100

    fix lne 2 : one parking is missing

--- src/services/parking.ts
+++ src/services/parking.ts
@@ -14,18 +14,19 @@ export async function fetchParkingsByStations(
 
   stations.forEach((station) => {
     const nom_station = station['nom']
-    const parking_on_station = parkingsFeatures.filter(
+    const parkings_on_station = parkingsFeatures.filter(
       (feature) => feature.getProperty('arret_nom') === nom_station
     )
-    if (parking_on_station.length > 0) {
-      const feature = parking_on_station[0]
-      parkings.push({
-        id: feature.get('id'),
-        arret_nom: feature.get('arret_nom'),
-        li_code: feature.get('li_code'),
-        nom: feature.get('nom'),
-        nb_max_places: feature.get('nb_max_places'),
-        nb_min_places: feature.get('nb_min_places'),
+    if (parkings_on_station.length > 0) {
+      parkings_on_station.forEach((feature) => {
+        parkings.push({
+          id: feature.get('id'),
+          arret_nom: feature.get('arret_nom'),
+          li_code: feature.get('li_code'),
+          nom: feature.get('nom'),
+          nb_max_places: feature.get('nb_max_places'),
+          nb_min_places: feature.get('nb_min_places'),
+        })
       })
     }
   })
