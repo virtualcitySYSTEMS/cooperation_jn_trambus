b2d1929a4b70824a30e113fef5be7fc44565b178
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-16 16:08:18 +0100

    carroussel timelines on board of side panel

--- src/components/home/TravelTimes.vue
+++ src/components/home/TravelTimes.vue
@@ -17,33 +17,36 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="flex items-center p-0 gap-2.5">
-    <div class="flex flex-col items-start gap-3 pt-0 pr-9 pb-0 pl-0">
-      <h2 class="font-dm-sans font-bold text-lg leading-6">
-        Vos futurs temps de parcours
-      </h2>
-      <UiOverflowContainer class="w-[402px]">
-        <div class="flex flex-row items-start gap-2">
-          <UiTravelTime
-            class="w-80 flex-none"
-            v-for="travelTime in state.travelTimes"
-            :key="travelTime.line"
-            :newDuration="travelTime.new"
-            :oldDuration="travelTime.old"
-            :lineNumber="travelTime.line"
-            :startStation="travelTime.start"
-            :endStation="travelTime.end"
-          >
-          </UiTravelTime>
-        </div>
-      </UiOverflowContainer>
-      <UiLinkPrimary
-        :url="'/traveltimes'"
-        :arrowStrokeColor="'stroke-red-600'"
-        :underlineColor="'bg-red-600'"
-        :title="'Voir plus de futurs temps de parcours'"
-        >Voir plus</UiLinkPrimary
-      >
-    </div>
-  </section>
+  <h2 class="font-dm-sans font-bold text-lg leading-6">
+    Vos futurs temps de parcours
+  </h2>
+
+  <div class="flex flex-col items-start gap-3 pt-0 pr-9 pb-0 pl-0">
+    <UiOverflowContainer class="w-[450px] -mx-6">
+      <div class="flex flex-row items-start gap-2 w-[450px]">
+        <UiTravelTime
+          class="w-80 flex-none"
+          v-for="travelTime in state.travelTimes"
+          :key="travelTime.line"
+          :newDuration="travelTime.new"
+          :oldDuration="travelTime.old"
+          :lineNumber="travelTime.line"
+          :startStation="travelTime.start"
+          :endStation="travelTime.end"
+        >
+        </UiTravelTime>
+      </div>
+    </UiOverflowContainer>
+  </div>
+
+  <UiLinkPrimary
+    :url="'/traveltimes'"
+    :arrowStrokeColor="'stroke-red-600'"
+    :underlineColor="'bg-red-600'"
+    :title="'Voir plus de futurs temps de parcours'"
+  >
+    Voir plus
+  </UiLinkPrimary>
+
+  <div class="border-b border-neutral-300 mb-3"></div>
 </template>
