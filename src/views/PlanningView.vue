d38ea9b93ac7491009f96e1954e3fe5235feb193
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-28 11:55:07 +0100

    reponsive planning header

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -10,16 +10,19 @@ const panelStore = usePanelsStore()
 
 <template>
   <main class="h-screen flex flex-col">
-    <div class="h-32 p-4 bg-white flex flex-row gap-2 items-center shadow-xl">
+    <div
+      class="p-4 bg-white flex flex-row gap-2 items-center shadow-xl overflow-x-auto overflow-y-hidden"
+    >
       <UiButton
         class="shadow-md rounded-lg p-2 flex gap-2.5 shrink-0 grow-0 mr-2"
         @click="panelStore.isPlanningViewShown = false"
       >
         <img :src="ChevronArrowRight" />
       </UiButton>
-      <div class="font-dm-sans font-bold text-2xl w-74 grow">
+      <div class="font-dm-sans grow font-bold text-2xl">
         Planning d’aménagement du réseau trambus
       </div>
+
       <div class="grow flex justify-end">
         <TimelineController> </TimelineController>
       </div>
